60a580c91e197e5e2ac52eb6af51cf4b23d7f906
1337CodeGod <51419091+1337CodeGod@users.noreply.github.com>
2022-12-22 20:31:23 -0500

    advent-of-code-2022-day-1

---
+++ app/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Day1();
+        Console.WriteLine("\nready");
+        Console.Read();
+    }
+ 
+    static void Day1()
+    {
+        List<int> elves = new();
+        int i = 0;
+ 
+        foreach (var line in File.ReadLines("input_1.txt"))
+        {
+            if (int.TryParse(line, out var n))
+                i += n;
+            else
+            {
+                elves.Add(i);
+                i = 0;
+            }
+        }
+ 
+        elves.Sort();
+        Console.WriteLine(elves.Max()); //part 1
+        Console.WriteLine(elves.TakeLast(3).Sum()); //part 2
+    }
+}
